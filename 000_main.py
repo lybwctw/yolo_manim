e1083aecb50d5a3a96849359df8ed894e0996fb9
lyb <lybwctw@gmail.com>
2026-02-02 21:30:46 +0800

    mod on frame

--- 000_main.py
+++ 000_main.py
@@ -4,48 +4,51 @@ from utils.image_raw import ImageRaw
 from utils.image_annotation import ImageAnnotation
 from utils.arrow_qmark import ArrowQmark
 from utils.digit_tile import DigitTile
-from utils.digit_layer_fake import DigitLayerFake
+from utils.digit_layer_fake import MDigitLayerFake, DigitLayerFake
 
 image_path = None
 label_path = None
 
+# FIXME, ALOT OF examples to explain
 class MainScene(Scene):
     def construct(self) -> None:
         # ************************************************************
         # TODO 1-1. introduce object detection problem
+        # ------------------------------------------------------------
+        # ImageRaw       ...>        annotation_final
         # ************************************************************
         self.next_section(
             'init assets',
             skip_animations=True,
         )
         manager = VGroup()
-        image = ImageRaw(image_path).scale(2.)
-        annotation = ImageAnnotation(image, label_path)
+        image_raw = ImageRaw(image_path).scale(2.)
+        annotation_final = ImageAnnotation(image_raw, label_path)
         aq1 = ArrowQmark(DOWN, LEFT).scale(.6).shift(LEFT*10)
         aq2 = ArrowQmark(UP, RIGHT).scale(.6).shift(RIGHT*10)
         aq3 = ArrowQmark(RIGHT, UP).scale(.6).shift(UP*.5)
-        dt1 = DigitTile('一堆\n数字').shift(DOWN*10)
-        dt2 = DigitTile('一堆\n数字').shift(DOWN*10)
+        dt_input = DigitTile('一堆\n数字').shift(DOWN*10)
+        dt_output = DigitTile('一堆\n数字').shift(DOWN*10)
         manager.add(
-            *[image, aq3, annotation]
+            *[image_raw, aq3, annotation_final]
         )
 
         # ------------------------------------------------------------
         self.next_section(
-            'create centered image and annotation',
+            'create centered image_raw and annotation_final',
             skip_animations=True,
         )
-        # self.play(Write(image))
-        # self.play(Write(annotation))
+        # self.play(Write(image_raw))
+        # self.play(Write(annotation_final))
         self.add(manager)
         self.wait()
 
         # ------------------------------------------------------------
         self.next_section(
-            'expand into image -> annotation',
+            'expand into image_raw -> annotation_final',
             skip_animations=True,
         )
-        manager.save_state()
+        # manager.save_state()
         manager.generate_target()
         manager.target[0].scale(.5)
         manager.target[2].scale(.5)
@@ -66,9 +69,9 @@ class MainScene(Scene):
         # ************************************************************
         # TODO 1-2. convert one problem into three
         # ------------------------------------------------------------
-        # ImageRaw                  Annotation
+        # ImageRaw                  annotation_final
         #    ->                         ->
-        # DigitTile       =>        DigitTile
+        # DigitTile       ...>        DigitTile
         # ************************************************************
         self.next_section(
             'introduce digital game idea, the big map',
@@ -77,7 +80,7 @@ class MainScene(Scene):
         manager[1] = VMobject()
         manager.add(
             *[aq1, VMobject(), aq2],
-            *[dt1, aq3, dt2],
+            *[dt_input, aq3, dt_output],
         )
         manager.generate_target()
         manager.target.arrange_in_grid(
@@ -89,19 +92,19 @@ class MainScene(Scene):
 
         # ------------------------------------------------------------
         self.next_section(
-            'maybe, realtime detection loop until demo image/annotation',
+            'maybe, realtime detection loop until demo image_raw/annotation_final',
             skip_animations=True,
         )
 
         # ************************************************************
-        # TODO 1-3. digitalize image input
+        # TODO 1-3. digitalize image_raw input
         # ------------------------------------------------------------
-        # ImageRaw                  Annotation
-        #    |                          |
-        # DigitLayerFake    =>      DigitTile
+        # ImageRaw                   annotation_final
+        #    |                           |
+        # MDigitLayerFake    ...>      DigitTile
         # ************************************************************
         self.next_section(
-            'focus on raw image input',
+            'focus on raw image_raw input',
             skip_animations=True,
         )
         manager.save_state()
@@ -126,7 +129,7 @@ class MainScene(Scene):
 
         # ------------------------------------------------------------
         self.next_section(
-            'reverse back from focused image to the big map',
+            'reverse back from focused image_raw to the big map',
             skip_animations=True,
         )
         self.play(manager.animate.restore())
@@ -134,90 +137,234 @@ class MainScene(Scene):
 
         # ------------------------------------------------------------
         self.next_section(
-            'tranfrom dt1 (digit tile) -> dlf1 (fake digit layer)',
+            'tranfrom mdlf_input_raw from dt to mdlf',
             skip_animations=False,
         )
-        dlf1 = DigitLayerFake(
+        mdlf_input_raw = MDigitLayerFake(
             n=3,
-            width=image.width,
-            height=image.height,
+            width=image_raw.width,
+            height=image_raw.height,
+            width_nominal=640,
+            height_nominal=360,
             buff=0.1,
-        ).move_to(dt1)
+        ).move_to(dt_input)
         self.play(
-            Transform(dt1, dlf1),
+            ReplacementTransform(dt_input, mdlf_input_raw),
+        )
+        self.wait()
+        self.play(
+            mdlf_input_raw.show_passing_flash(),
+        )
+        self.play(
+            mdlf_input_raw.unwrite_shape_texts(),
         )
         self.wait()
 
         # ************************************************************
-        # TODO 1-4. digitalize annotation output
+        # TODO 1-4. digitalize annotation_final output
         # ------------------------------------------------------------
-        # ImageRaw                  Annotation
-        #    |                          |
-        # DigitLayerFake    =>      DigitLayerFake
+        # ImageRaw                     annotation_final
+        #    |                           |
+        # MDigitLayerFake    ...>      DigitLayerFake
         # ************************************************************
         self.next_section(
-            'focus on annotation',
+            'focus on annotation_final',
+            skip_animations=False,
+        )
+        manager.save_state()
+        manager.generate_target()
+        manager.target.arrange_in_grid(rows=3, cols=3, buff=6.)
+        manager.target.shift(-manager.target[2].get_center())
+        manager.target[2].scale(2.)
+        self.play(MoveToTarget(manager))
+        self.wait()
+
+        # ------------------------------------------------------------
+        self.next_section(
+            '002_annotation_zoom, explain the concept of annotation_final',
             skip_animations=True,
         )
 
         # ------------------------------------------------------------
         self.next_section(
-            '',
-            skip_animations=True,
+            'reverse back from focused annotation_final to the big map',
+            skip_animations=False,
         )
+        self.play(manager.animate.restore())
+        self.wait()
+
+        # ------------------------------------------------------------
+        self.next_section(
+            'tranfrom dlf_output_final from dt to dlf',
+            skip_animations=False,
+        )
+        dlf_output_final = DigitLayerFake(
+            width=1.,
+            height=1.5,
+            width_nominal=5,
+            height_nominal=8,
+        ).move_to(dt_output)
+        self.play(
+            Transform(dt_output, dlf_output_final),
+        )
+        self.wait()
+        self.play(
+            dlf_output_final.show_passing_flash(),
+        )
+        self.play(
+            dlf_output_final.unwrite_shape_texts(),
+        )
+        self.wait()
 
         # ************************************************************
         # TODO 1-5. two preferences of modern AI
         # ------------------------------------------------------------
         #
         # ************************************************************
+        self.next_section(
+            'the changing shapes of current input & output',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'the magnitude of current input & output',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'try fixing current design of input & output',
+            skip_animations=False,
+        )
 
         # ************************************************************
-        # TODO 1-6. redigitalize image input
+        # TODO 1-6. redesign image_raw input
         # ------------------------------------------------------------
-        # IR   ->  IR                Ann  ->  Ann
-        #  |        |                 |        |
-        # DLF  ->  DLF  ->  DLF  =>  DLF  ->  DLF
+        #  IR   ->   IR                   Ann  ->  Ann
+        #  |         |                     |        |
+        # MDLF  ->  MDLF  ->  MDLF  ...>  DLF  ->  DLF
         # ------------------------------------------------------------
-        # IR                         Ann
-        #      ->                ->
-        #          DLF  =>  DLF
+        #  IR                                      Ann
+        #          <prep>                    <post>
+        #                     MDLF  ...>  DLF
         # ************************************************************
+        image_repad = VMobject()
+        mdlf_input_repad = VMobject()
+        mdlf_input_norm = VMobject()
+        annotation_repad = VMobject()
+        dlf_output_repad = VMobject()
+        
+        self.next_section(
+            'focus on image_raw and mdlf_input_raw',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'resize image_raw',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'resize mdlf_input_raw',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'pad image_raw',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'pad mdlf_input_raw',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'normalize mdlf_input_raw',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'simplify into preprocessing step',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'reverse back to the big map',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'introduce postprocess on dlf_output_final',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'fix shape and small mag after preprocessing',
+            skip_animations=False,
+        )
 
         # ************************************************************
-        # TODO 1-7. redigitalize annotation output
+        # TODO 1-7. redigitalize annotation_final output
         #      reference of YOLO versions, including YOLO26
         # ------------------------------------------------------------
-        # IR                                              Ann
-        #      ->                                     ->
-        #          DLF   =>  DLF*2  ->  DLF  ->  DLF
+        #  IR   ->   IR                               Ann  ->  Ann  ->  Ann
+        #  |         |                                 |        |        |
+        # MDLF  ->  MDLF  ->  MDLF  ...>  MDLF*2  ->  DLF  ->  DLF  ->  DLF
+        # ------------------------------------------------------------
+        #  IR                                                           Ann
+        #          <prep>                                     <post>
+        #                     MDLF  ...>  MDLF*2  ->  DLF
         # ************************************************************
+        mdlf_output_32_box = VMobject()
+        mdlf_output_32_cls = VMobject()
+        dlf_output_32_decode = VMobject()
+        annotation_32_decode = VMobject()
+
+        self.next_section(
+            'focus on annotation_repad and dlf_output_repad',
+            skip_animations=False,
+        )
+
+        self.next_section(
+            'focus on annotation_repad',
+            skip_animations=False,
+        )
 
         # ************************************************************
-        # TODO 1-8. multi-layer annotation output
+        # TODO 1-8. multi-layer annotation_final output
+        # FIXME, explicitly contain the step of concat on 32/16/8?
         # ------------------------------------------------------------
-        # IR                                                      Ann
-        #      ->           DLF*2  ->  DLF                    ->
-        #          DLF  =>  DLF*2  ->  DLF  ->  DLF  ->  DLF
-        #                   DLF*2  ->  DLF
+        #  IR   ->   IR                                        Ann  ->  Ann
+        #  |         |                     MDLF*2  ->  DLF      |        |
+        # MDLF  ->  MDLF  ->  MDLF  ...>  MDLF*2  ->  DLF  ->  DLF  ->  DLF
+        #                                MDLF*2  ->  DLF
+        # ------------------------------------------------------------
+        #  IR                                                           Ann
+        #          <prep>                  MDLF*2  ->  DLF    <post>
+        #                     MDLF  ...>  MDLF*2  ->  DLF
+        #                                MDLF*2  ->  DLF
         # ************************************************************
+        mdlf_output_16_box = VMobject()
+        mdlf_output_16_cls = VMobject()
+        dlf_output_16_decode = VMobject()
+        annotation_16_decode = VMobject()
+
+        mdlf_output_8_box = VMobject()
+        mdlf_output_8_cls = VMobject()
+        dlf_output_8_decode = VMobject()
+        annotation_8_decode = VMobject()
+
+        self.next_section(
+            '',
+            skip_animations=False,
+        )
 
         # ************************************************************
         # FIXME 1-9. [visual thinking] VS [digital thinking]
         # ------------------------------------------------------------
-        # IR  -> IR                                   Ann -> Ann
-        #  |      |            DLF*2 -> DLF            |      |
-        # DLF -> DLF -> DLF => DLF*2 -> DLF -> DLF -> DLF -> DLF
-        #                      DLF*2 -> DLF
-        # ------------------------------------------------------------
-        # IR                                           Ann
-        #      <pre>           DLF*2           <post>
-        #             DLF  =>  DLF*2  ->  DLF
-        #                      DLF*2
-        # ************************************************************
 
         # ************************************************************
-        # TODO 2-1. [tensor] [model/network] [module]
+        # TODO 2-1. [tensor] [model/network] [module/layer] [parameter]
         # ------------------------------------------------------------
         #                                                     MC*2
         # MC    =>     MC    =>     MC    =>     MC    =>     MC*2
--- utils/digit_layer_fake.py
+++ utils/digit_layer_fake.py
@@ -1,7 +1,60 @@
 from manim import *
 
-# DigitLayer without real data inside
 class DigitLayerFake(VGroup):
+    def __init__(
+        self,
+        width=2,
+        height=4,
+        width_nominal=None,
+        height_nominal=None,
+    ):
+        # TODO, consider using config as a single arg
+        super().__init__()
+        self.w = width_nominal if width_nominal else width
+        self.h = height_nominal if height_nominal else height
+        rect = Rectangle(
+            width=width,
+            height=height,
+            fill_color=MAROON,
+            fill_opacity=0.5,
+        )
+        self.rect = rect
+        self.shape_texts = VGroup()
+        self.add(self.rect)
+
+    def show_passing_flash(self):
+        path = VMobject()
+        path.set_points_as_corners([
+            self.rect.get_corner(LEFT+DOWN),
+            self.rect.get_corner(LEFT+UP),
+            self.rect.get_corner(RIGHT+UP),
+        ]).set_stroke(color=BLUE)
+        text_h = Text(str(self.h),font_size=25).next_to(self.rect,LEFT)
+        text_w = Text(str(self.w),font_size=25).next_to(self.rect,UP)
+        self.shape_texts = VGroup(text_h, text_w)
+        anim = AnimationGroup(
+            ShowPassingFlash(
+                path,
+                run_time=2.,
+                time_width=2.,
+            ),
+            AnimationGroup(
+                *(Write(text) for text in self.shape_texts),
+                lag_ratio=0.8,
+            )
+        )
+
+        return anim
+
+    def unwrite_shape_texts(self):
+        anim = AnimationGroup(
+            *(Unwrite(text) for text in self.shape_texts),
+            lag_ratio=0.8,
+        )
+        return anim
+
+# multiple DigitLayerFake as a whole
+class MDigitLayerFake(VGroup):
     def __init__(
         self,
         n=3,
