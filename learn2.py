a99c8959546d584e262e58fd56a1b9c034f47536
LYB <lybwctw@gmail.com>
2026-03-31 15:00:24 +0800

    updates

--- learn2.py
+++
@@ -1,9 +0,0 @@
-from manim import *
-import numpy as np
-
-class TableExamples(ThreeDScene):
-    def construct(self):
-        sq = Cube(stroke_width=2, stroke_opacity=1.0, fill_color=BLACK)
-        self.play(Write(sq))
-        self.move_camera(phi=60*DEGREES, theta=-75*DEGREES)
-        self.wait()
