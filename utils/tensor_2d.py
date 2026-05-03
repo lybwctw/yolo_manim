d7dc1de8788e84246e5c6fe903b44b5dad3d2dfa
LYB <lybwctw@gmail.com>
2026-05-03 16:13:18 +0800

    updates

--- utils/tensor_2d.py
+++ utils/tensor_2d.py
@@ -13,55 +13,24 @@ DECIMAL_CONFIG = {
     'color': WHITE,
 }
 
-ARRANGE_CONFIG = {
-    'cell_width': 0.8,
-    'cell_height': 0.4,
-}
-
 # empirically determined, for monospaced font
-FONT_SIZE_WIDTH_RATIO = 0.063
-FONT_SIZE_HEIGHT_RATIO = 0.020
+FONT_SIZE_WIDTH_RATIO = 0.06
+FONT_SIZE_HEIGHT_RATIO = 0.02
 
-# also used by mcubes
-# TODO, make these functions general
-def _indices_from_part(part, size):
-    # np.int64 for ijk from np.argwhere(...)
-    if isinstance(part, (int, np.int64)):
-        return [part]
-    if isinstance(part, slice):
-        start, stop, step = part.indices(size)
-        return list(range(start, stop, step))
-    raise TypeError(f"Invalid index: {part}")
+# helper functions
 
-def _normalize_idx(idx):
-    if not isinstance(idx, tuple):
-        idx = (idx,)
-    return idx
-def _expand_ellipsis(idx, ndim):
-    if Ellipsis not in idx:
-        return idx
-
-    pos = idx.index(Ellipsis)
-    missing = ndim - (len(idx) - 1)
-    return (
-        idx[:pos]
-        + (slice(None),) * missing
-        + idx[pos + 1 :]
-    )
-def _fill_missing_dims(idx, ndim):
-    return idx + (slice(None),) * (ndim - len(idx))
 
 class Tensor2D(VMobject):
     def __init__(
         self,
-        data: list | None = None,   # [(a, n), (b, n), (c, n), ...]
+        data: list | None = None,   # [(n, a), (n, b), (n, c), ...]
         decimal_config: dict = {},
         arrange_config: dict = {},
     ):
         super().__init__()
         # TODO, make sure all dim is the same except the last
         self.formatters, self.col_ratios = self._create_formatters(data)
-        self.data = np.concat(data, axis=-1)
+        self.data = np.concat(data, axis=-1)    # (n,7)
         self.shape = self.data.shape
         self.ndim = self.data.ndim
         self.decimal_config = {**DECIMAL_CONFIG, **decimal_config}
@@ -71,7 +40,7 @@ class Tensor2D(VMobject):
         }
         self.arrange_config = {**auto_arrange_config, **arrange_config}
 
-        self.decimals = self._create_decimals()
+        self.objs = self._create_objs()
         self.mobs = self._create_mobs()
         self._arrange_mobs()
 
@@ -79,7 +48,7 @@ class Tensor2D(VMobject):
     
     def _create_formatters(
         self,
-        group,
+        group: list,
     ):
         """Compute formatter for each col.
            Compute col ratio for each col.
@@ -90,7 +59,6 @@ class Tensor2D(VMobject):
             if np.issubdtype(data.dtype, np.integer):
                 formatter = ['{:>3.0f}'] * data.shape[1]
                 col_ratio = [0.8] * data.shape[1]
-                col_ratio[-1] += 0.23     # a little bit more on first of next
             elif np.issubdtype(data.dtype, np.floating):
                 formatter = ['{:.2f}'] * data.shape[1]
                 col_ratio = [1.0] * data.shape[1]
@@ -98,7 +66,7 @@ class Tensor2D(VMobject):
             cs = cs + col_ratio
         return fs, cs
 
-    def _create_decimals(
+    def _create_objs(
         self,
     ) -> list:
         """Create a list of list of decimal vmobject.
@@ -111,7 +79,6 @@ class Tensor2D(VMobject):
             for i,d in enumerate(row):
                 mob = Text(
                     self.formatters[i].format(d),
-                    # f'{d:.2f}',
                     **self.decimal_config,
                 )
                 row_mobs.append(mob)
@@ -121,9 +88,9 @@ class Tensor2D(VMobject):
     def _create_mobs(
         self,
     ) -> VGroup:
-        """Create a vgroup of vgroup of Text based on decimals.
+        """Create a vgroup of vgroup of Text based on objs.
         """
-        return VGroup( *(VGroup(*row) for row in self.decimals))
+        return VGroup( *(VGroup(*row) for row in self.objs))
     
     def _arrange_mobs(
         self,
@@ -135,77 +102,384 @@ class Tensor2D(VMobject):
         for i, row in enumerate(self.mobs):
             for j, mob in enumerate(row):
                 dy = i * cfg['cell_height'] * DOWN
-                dx = sum(self.col_ratios[:j]) * cfg['cell_width'] * RIGHT
+                dx = sum(self.col_ratios[:j+1]) * cfg['cell_width'] * RIGHT
                 mob.align_to(dy, DOWN)
                 mob.align_to(dx, RIGHT)
             
         self.mobs.center()  # to origin after arrange
         return self
 
-    def __getitem__(self, idx):
-        idx = _normalize_idx(idx)
-        idx = _expand_ellipsis(idx, self.ndim)
-        idx = _fill_missing_dims(idx, self.ndim)
+    def __getitem__(
+        self,
+        idx,
+    ) -> VMobject:
+        """Indexing utils.
+           Prerequisites:
+                ndims, dimensions of data
+                shape, shape of data
+                mobs, ???
+           NOTE, general naming as self.objs
+        """
+        # normalize idx
+        if not isinstance(idx, tuple):
+            idx = (idx,)
+
+        # expand ellipsis
+        if Ellipsis in idx:
+            pos = idx.index(Ellipsis)
+            missing = self.ndim - (len(idx) - 1)
+            idx = (
+                idx[:pos]
+                + (slice(None),) * missing
+                + idx[pos + 1 :]
+            )
+
+        # fill missing dims
+        idx = idx + (slice(None),) * (self.ndim - len(idx))
 
         if len(idx) != self.ndim:
             raise IndexError("Invalid index dimension")
 
-        resolved = [
-            _indices_from_part(part, size)
-            for part, size in zip(idx, self.shape)
-        ]
+        resolved = []
+        for part, size in zip(idx, self.shape):
+            if isinstance(part, (int, np.int64)):
+                data = [part]
+            elif isinstance(part, slice):
+                start, stop, step = part.indices(size)
+                data = list(range(start, stop, step))
+            else:
+                raise TypeError(f"Invalid index: {part}")
+            resolved.append(data)
 
         keys = list(itertools.product(*resolved))
 
         if not keys:
             return VGroup()
+        
+        # FIXME, ugly design for list of list
+        def nested_get(obj, idx_tuple):
+            for i in idx_tuple:
+                obj = obj[i]
+            return obj
 
         if len(keys) == 1:
-            i, j = keys[0]
-            return self.decimals[i][j]
+            return nested_get(self.objs, keys[0])
+        return VGroup(*(nested_get(self.objs, k) for k in keys))
 
-        return VGroup(*(self.decimals[i][j] for i, j in keys))
+    def apply_whole_take_max(
+        self,
+        aargs: dict = {},
+        gargs: dict = {},
+    ) -> Animation:
+        """For all rows.
+           xyxyccc -> xyxyc
+        """
+        anims = AnimationGroup(
+            *(self.apply_row_take_max(
+                i,
+                **aargs
+            ) for i in range(self.shape[0])),
+            **gargs,
+        )
+
+        # FIXME: update data and shape manually, including cls
+        self.data = self.data[:, :6]
+        self.shape = self.data.shape
+
+        # FIXME: update formatters and col_ratios, including cls
+        self.formatters = self.formatters[:5] + [self.formatters[0]]
+        self.col_ratios = self.col_ratios[:5] + [0.6]
+
+        return anims
+    
+    def apply_row_take_max(
+        self,
+        n: int,
+        **aargs,
+    ) -> Animation:
+        """For a single row.
+           xyxyccc -> xyxyc
+        """
+        # preserve max conf and cls only, with garbage tail
+        row = self.data[n]
+        row_tail = row[4:]
+        local_idx = row_tail.argmax()
+        global_idx = local_idx + 4
+        max_val = row_tail[local_idx]
+        row[4] = max_val        # save max conf
+        row[5] = local_idx      # save cls index
+        
+        # update objs and mobs
+        row_objs = self.objs[n]
+        keep_obj = row_objs[global_idx]
+        keep_indices = set(list(range(4)) + [global_idx])
+        self.objs[n] = row_objs[:4] + [row_objs[global_idx]]
+        rm_objs = [
+            obj for j, obj in enumerate(row_objs)
+            if j not in keep_indices
+        ]
+        self.mobs.remove(
+            *rm_objs
+        )
+
+        # shift and unwrite animations
+        anims = [
+            keep_obj.animate.move_to(
+                row_objs[4],
+                aligned_edge=(UL),
+            )
+        ]
+        for obj in rm_objs:
+            anim = Uncreate(obj)
+            anims.append(anim)
+        return AnimationGroup(
+            *anims,
+            **aargs,
+        )
+
+    def apply_whole_append_cls(
+        self,
+        aargs: dict = {},
+        gargs: dict = {},
+    ) -> Animation:
+        """For all rows.
+        xyxyc -> xyxycC
+        """
+        anims = AnimationGroup(
+            *(self.apply_row_append_cls(
+                i,
+                **aargs,
+            ) for i in range(self.shape[0])),
+            **gargs,
+        )
+
+        # data and shape already updated
+        # formatters and col_ratios already updated
+
+        return anims
+    
+    def apply_row_append_cls(
+        self,
+        n: int,
+        **aargs,
+    ) -> Animation:
+        """For a single row.
+           Append cls text.
+           data is already updated.
+        """
+        # FIXME, use realtime font size
+        mob = Text(
+            self.formatters[5].format(self.data[n,5]),
+            **self.decimal_config,
+        )
+        mob.move_to(self[n,-1], aligned_edge=RIGHT)
+        mob.shift(RIGHT * self.col_ratios[5] * self.arrange_config['cell_width'])
+
+        # update objs and mobs
+        self.objs[n].append(mob)
+        self.mobs.add(mob)      # TODO: rearrange??
+
+        anim = Create(
+            mob,
+            **aargs,
+        )
+        return anim
+    
+    def apply_whole_filter_conf(
+        self,
+        conf: float = 0.5,
+        aargs: dict = {},
+        gargs: dict = {},
+    ) -> Animation:
+        """For all rows.
+           Keep or remove.
+        """
+        anims = AnimationGroup(
+            *(self.apply_row_filter_conf(
+                i, conf=conf, **aargs,
+            ) for i in range(self.shape[0])),
+            **gargs,
+        )
+
+        # update objs and mobs
+        objs_to_keep = [row for row, k in zip(self.objs, self.keep) if k]
+        objs_to_remove = [row for row, k in zip(self.objs, self.keep) if not k]
+        self.objs = objs_to_keep
+        for row in objs_to_remove:
+            self.mobs.remove(*row)
+
+        # update data and shape
+        self.data = self.data[self.keep]
+        del self.keep
+        self.shape = self.data.shape
+
+        return anims
+
+    def apply_row_filter_conf(
+        self,
+        n: int,
+        conf: float = 0.5,  # conf threshold
+        **aargs,
+    ) -> Animation:
+        """For a single row.
+           keep or remove.
+        """
+        gap = aargs.pop('gap', 1.0)
+
+        row = self[n]
+        keep = self.data[n,4] > conf
+
+        # maintian a keep member for later clean job
+        if not hasattr(self, 'keep'):
+            self.keep = np.zeros(self.shape[0], dtype=bool)
+            # FIXME: remember current y and left
+            self.orig_y = self.get_y()
+            self.orig_left = self.get_corner(LEFT)
+        self.keep[n] = keep
+
+        if keep:
+            run_time = aargs.get('run_time', 1.0)
+            aargs['run_time'] = 2*run_time
+            anim = ApplyFunction(
+                    lambda mob: mob.scale(1.5).set_color(PURE_GREEN),
+                    row,
+                    rate_func=rate_functions.there_and_back,
+                    **aargs,
+                )
+        else:
+            anim = Succession(
+                ApplyFunction(
+                    lambda mob: mob.scale(1.5).set_color(PURE_RED),
+                    row,
+                    **aargs,
+                ),
+                Wait(gap),
+                Uncreate(row, **aargs),
+            )
+        return anim
+    
+    def rearrange_after_filter(
+        self,
+        buff: float | None = None,
+        **aargs,
+    ) -> Animation:
+        """Rearrange current rows.
+           Used after filter function.
+        """
+        buff = buff or self.arrange_config['cell_height']
+
+        rows = VGroup()
+        for i in range(self.shape[0]):
+            rows.add(self[i])
+        rows.generate_target()
+
+        for i, row in enumerate(rows.target):
+            # FIXME, use adapted cell height
+            dy = i * self.arrange_config['cell_height'] * DOWN
+            row.align_to(dy, DOWN)
+        rows.target.set_y(self.orig_y)
+        rows.target.align_to(self.orig_left, LEFT)
+
+        # clean members?
+        del self.orig_y
+        del self.orig_left
+
+        anim = MoveToTarget(
+            rows, **aargs,
+        )
+
+        return anim
+    
+    def sort(
+        self,
+        reversed: bool = True, # 1->0 by default
+    ) -> Animation:
+        """Animation to sort rows according to conf.
+           Used before NMS.
+        """
+        pass
+    
+    def split_into_classes(
+        self,
+        direction: np.ndarray = DOWN,
+    ) -> list:      # -> (n,6)*[3|2|1]
+        pass
+
+    def apply_whole_filter_nms(
+        self,
+    ) -> Animation:
+        pass
     
 class Demo(Scene):
     def construct(self):
-        t1 = Tensor2D(
+        tensor = Tensor2D(
             data=[
-                np.random.randint(0,999,(80,4)),
-                np.random.uniform(0,1,(80,3))
+                np.random.randint(0,999,(20,4)),
+                np.random.uniform(0,1,(20,3))
             ],
             decimal_config={
-                'font_size': 4.3,
+                'font_size': 14.3,
                 'color': WHITE,
             },
-            # arrange_config={
-            #     'buff_row': 0.03,
-            #     'buff_col': 0.10,
-            # },
+            arrange_config={
+                'cell_width': 0.55,
+                'cell_height': 0.3,
+            },
         )
-        # t1 = Tensor2D(
-        #     data=[
-        #         np.random.randint(0,999, (10,4)),
-        #         np.random.uniform(0,1, (10,3)),
-        #     ],
-        #     decimal_config={
-        #         'font_size': 15.5,
-        #         'color': WHITE,
-        #     },
-        #     # arrange_config={
-        #     #     'cell_width': 1.8,
-        #     #     'cell_height': 1.4,
-        #     # },
-        # )
 
-        self.add(t1)
+        self.play(Create(
+            tensor,
+            lag_ratio=0.1,
+            run_time=0.5,
+        ))
         self.wait()
-        # self.play(Write(
-        #     t1,
-        #     lag_ratio=0.1,
-        #     run_time=1.5,
-        # ))
-        # self.wait()
 
-        partial = t1[:,-3:]
-        self.play(partial.animate.set_color(RED))
+        self.play(AnimationGroup(
+            tensor[:,4].animate.set_color(RED),
+            tensor[:,5].animate.set_color(GREEN),
+            tensor[:,6].animate.set_color(BLUE),
+        ))
+        self.wait()
+
+        self.play(tensor.apply_whole_take_max(
+            aargs={},
+            gargs={'lag_ratio': 0.5, 'run_time': 0.5}
+        ))
+        # self.play(AnimationGroup(
+        #     *(tensor.apply_row_take_max(i) for i in range(tensor.shape[0])),
+        #     lag_ratio=0.5,
+        #     run_time=2.0,
+        # ))
+        self.wait()
+
+        self.play(tensor.apply_whole_append_cls(
+            aargs={},
+            gargs={'lag_ratio': 0.5, 'run_time': 0.5,},
+        ))
+        self.wait()
+
+        self.play(tensor[:,4].animate(
+            lag_ratio=0.5,
+            run_time=0.5,
+        ).set_color(WHITE))
+        self.wait()
+
+        # self.play(AnimationGroup(
+        #     *(tensor.apply_row_filter_conf(
+        #         i, conf=0.5, run_time=0.5, gap=0.5,
+        #     ) for i in range(tensor.shape[0])),
+        #     lag_ratio=0.5,
+        #     run_time=2.0,
+        # ))
+        self.play(tensor.apply_whole_filter_conf(
+            conf=0.7,
+            aargs={},
+            gargs={'lag_ratio':0.5, 'run_time':1.0},
+        ))
+        self.wait()
+
+        self.play(tensor.rearrange_after_filter(
+            run_time=1.0,
+            rate_func=rate_functions.ease_out_back,
+        ))
         self.wait()
