be8896ac72c96b272ce95f8ebc12a63d112c2132
LYB <lybwctw@gmail.com>
2026-06-26 16:39:54 +0800

    updates

--- 020_big_map.py
+++ 020_big_map.py
@@ -340,7 +340,7 @@ class MainScene(Scene):
         # ************************************************************
         self.next_section(
             "[4a] intuition view: scale back",
-            skip_animations=True,
+            skip_animations=False,
         )
         # ************************************************************
         # new arrow
@@ -356,7 +356,7 @@ class MainScene(Scene):
         ))
 
         # copy system
-        s32_back = s32_conf.copy() # FIXME: HERE EHRHERERERE
+        s32_back = s32_nms.copy() # FIXME: HERE EHRHERERERE
         self.play(s32_back.animate(
             run_time=wt,
         ).next_to(
@@ -364,6 +364,7 @@ class MainScene(Scene):
             RIGHT,
             buff=GAP_POSTPROCESS+0.3,   # more for scale up later
         ))
+
         s32_back[-1].apply_scale_back(
             scene=self,
             scale_factor=1.5,
@@ -526,143 +527,143 @@ class MainScene(Scene):
             skip_animations=False,
         )
         # ************************************************************
-        # new arrows
-        aci_14 = ArrowComment(False, RIGHT).scale(0.2).move_to(UP*5.0)
-        aci_15 = ArrowComment(False, RIGHT).scale(0.2).move_to(UP*5.0)
-        act_game = ArrowComment(False, RIGHT).scale(0.2).move_to(LEFT*10.0)
-        act_14 = ArrowComment(False, RIGHT).scale(0.2).move_to(DOWN*5.0)
-        act_15 = ArrowComment(False, RIGHT).scale(0.2).move_to(DOWN*5.0)
+        # # new arrows
+        # aci_14 = ArrowComment(False, RIGHT).scale(0.2).move_to(UP*5.0)
+        # aci_15 = ArrowComment(False, RIGHT).scale(0.2).move_to(UP*5.0)
+        # act_game = ArrowComment(False, RIGHT).scale(0.2).move_to(LEFT*10.0)
+        # act_14 = ArrowComment(False, RIGHT).scale(0.2).move_to(DOWN*5.0)
+        # act_15 = ArrowComment(False, RIGHT).scale(0.2).move_to(DOWN*5.0)
 
-        mobs = Group(
-            Mobject(), s32_offset, s32_prob, aci_14, s32_merged_2d, aci_15, s32_back,
-            act_game,  t32_offset, t32_prob, act_14, t32_merged_2d, act_15, t32_back,
-        )
-        mobs.generate_target()
-        mobs.target[4].scale(MERGED_SCALE_FACTOR)   # merged was scaled up
-        mobs.target[6].scale(MERGED_SCALE_FACTOR)   # merged was scaled up
-        mobs.target.arrange_in_grid(
-            rows=2,
-            cols=7,
-            buff=0.3,
-        ).center().scale(1.5)
-        self.play(AnimationGroup(
-            *(FadeOut(mob) for mob in (
-                mat_1,
-                s32_mid_decode, s32_mid_postprocess,
-                t32_mid_decode, t32_mid_postprocess,
-            )),
-            lag_ratio=0.0,
-            run_time=wt,
-        ))
-        self.play(MoveToTarget(
-            mobs,
-            run_time=wt,
-            rate_func=rate_functions.ease_out_back,
-        ))
-        self.wait(wt)
+        # mobs = Group(
+        #     Mobject(), s32_offset, s32_prob, aci_14, s32_merged_2d, aci_15, s32_back,
+        #     act_game,  t32_offset, t32_prob, act_14, t32_merged_2d, act_15, t32_back,
+        # )
+        # mobs.generate_target()
+        # mobs.target[4].scale(MERGED_SCALE_FACTOR)   # merged was scaled up
+        # mobs.target[6].scale(MERGED_SCALE_FACTOR)   # merged was scaled up
+        # mobs.target.arrange_in_grid(
+        #     rows=2,
+        #     cols=7,
+        #     buff=0.3,
+        # ).center().scale(1.5)
+        # self.play(AnimationGroup(
+        #     *(FadeOut(mob) for mob in (
+        #         mat_1,
+        #         s32_mid_decode, s32_mid_postprocess,
+        #         t32_mid_decode, t32_mid_postprocess,
+        #     )),
+        #     lag_ratio=0.0,
+        #     run_time=wt,
+        # ))
+        # self.play(MoveToTarget(
+        #     mobs,
+        #     run_time=wt,
+        #     rate_func=rate_functions.ease_out_back,
+        # ))
+        # self.wait(wt)
 
-        # ************************************************************
-        self.next_section(
-            'into bigger map: from input to output',
-            skip_animations=False,
-        )
-        # ************************************************************
-        aci_1 = aci_14.copy().move_to(LEFT*10)
-        act_1 = act_14.copy().move_to(LEFT*10)
-        sin_raw = s32_back[0].copy().set_opacity(1.0).move_to(LEFT*10)
-        sin_norm = s32_merged_2d[0].copy().set_opacity(1.0).move_to(LEFT*10)
-        tin_raw = LayersFake(
-            n=3,
-            ref=sin_raw,
-            expanded=True,
-            width_nominal=sin_raw.width_nominal,
-            height_nominal=sin_raw.height_nominal,
-            buff=0.05,              # TODO, natural buff?
-        ).scale(1.0).shift(LEFT*10) # TODO, scale up a little bit?
-        tin_norm = LayersFake(
-            n=3,
-            ref=sin_norm,
-            expanded=True,
-            width_nominal=sin_norm.width_nominal,
-            height_nominal=sin_norm.height_nominal,
-            buff=0.05,              # TODO, natural buff?
-        ).scale(1.0).shift(LEFT*10) # TODO, scale up a little bit?
+        # # ************************************************************
+        # self.next_section(
+        #     'into bigger map: from input to output',
+        #     skip_animations=False,
+        # )
+        # # ************************************************************
+        # aci_1 = aci_14.copy().move_to(LEFT*10)
+        # act_1 = act_14.copy().move_to(LEFT*10)
+        # sin_raw = s32_back[0].copy().set_opacity(1.0).move_to(LEFT*10)
+        # sin_norm = s32_merged_2d[0].copy().set_opacity(1.0).move_to(LEFT*10)
+        # tin_raw = LayersFake(
+        #     n=3,
+        #     ref=sin_raw,
+        #     expanded=True,
+        #     width_nominal=sin_raw.width_nominal,
+        #     height_nominal=sin_raw.height_nominal,
+        #     buff=0.05,              # TODO, natural buff?
+        # ).scale(1.0).shift(LEFT*10) # TODO, scale up a little bit?
+        # tin_norm = LayersFake(
+        #     n=3,
+        #     ref=sin_norm,
+        #     expanded=True,
+        #     width_nominal=sin_norm.width_nominal,
+        #     height_nominal=sin_norm.height_nominal,
+        #     buff=0.05,              # TODO, natural buff?
+        # ).scale(1.0).shift(LEFT*10) # TODO, scale up a little bit?
 
-        mobs = Group(
-            sin_raw, aci_1, sin_norm, Mobject(), s32_offset, s32_prob, aci_14, s32_merged_2d, aci_15, s32_back,
-            tin_raw, act_1, tin_norm, act_game,  t32_offset, t32_prob, act_14, t32_merged_2d, act_15, t32_back,
-        )
-        mobs.generate_target()
+        # mobs = Group(
+        #     sin_raw, aci_1, sin_norm, Mobject(), s32_offset, s32_prob, aci_14, s32_merged_2d, aci_15, s32_back,
+        #     tin_raw, act_1, tin_norm, act_game,  t32_offset, t32_prob, act_14, t32_merged_2d, act_15, t32_back,
+        # )
+        # mobs.generate_target()
 
-        # make output tensors smaller to match input tensors
-        mobs.target[14].scale(0.6)
-        mobs.target[15].scale(0.6)
-        mobs.target[17].scale(0.7)
-        mobs.target[19].scale(0.7)
-        mobs.target.arrange_in_grid(
-            rows=2,
-            cols=10,
-            # buff=0.5,
-        ).center().scale(0.95)
+        # # make output tensors smaller to match input tensors
+        # mobs.target[14].scale(0.6)
+        # mobs.target[15].scale(0.6)
+        # mobs.target[17].scale(0.7)
+        # mobs.target[19].scale(0.7)
+        # mobs.target.arrange_in_grid(
+        #     rows=2,
+        #     cols=10,
+        #     # buff=0.5,
+        # ).center().scale(0.95)
 
-        # FIXME: more gap on output tensor layers
-        mobs.target[14].adjust_gap(buff=0.05)
-        mobs.target[15].adjust_gap(buff=0.05)
-        # orig_center = mobs.target[14].get_center()
-        # mobs.target[14].rects.arrange(
-        #     UR,
-        #     buff=-mobs.target[14][0].width + 0.09,
-        # ).move_to(orig_center)
-        # orig_center = mobs.target[15].get_center()
-        # mobs.target[15].rects.arrange(
-        #    UR,
-        #     buff=-mobs.target[15][0].width + 0.08,
-        # ).move_to(orig_center)
+        # # FIXME: more gap on output tensor layers
+        # mobs.target[14].adjust_gap(buff=0.05)
+        # mobs.target[15].adjust_gap(buff=0.05)
+        # # orig_center = mobs.target[14].get_center()
+        # # mobs.target[14].rects.arrange(
+        # #     UR,
+        # #     buff=-mobs.target[14][0].width + 0.09,
+        # # ).move_to(orig_center)
+        # # orig_center = mobs.target[15].get_center()
+        # # mobs.target[15].rects.arrange(
+        # #    UR,
+        # #     buff=-mobs.target[15][0].width + 0.08,
+        # # ).move_to(orig_center)
 
-        # back to big map
-        self.play(MoveToTarget(
-            mobs,
-            run_time=wt,
-            rate_func=rate_functions.ease_out_back,
-        ))
-        self.wait()
+        # # back to big map
+        # self.play(MoveToTarget(
+        #     mobs,
+        #     run_time=wt,
+        #     rate_func=rate_functions.ease_out_back,
+        # ))
+        # self.wait()
 
-        # show shape on tensors
+        # # show shape on tensors
 
-        # ************************************************************
-        self.next_section(
-            'show shape on simplified output map',
-            skip_animations=False,
-        )
-        # ************************************************************
-        ac_all = VGroup(
-            aci_1,           aci_14, aci_15,
-            act_1, act_game, act_14, act_15
-        )
-        tensor_mobs = (
-            tin_raw, tin_norm, t32_offset, t32_prob, t32_merged_2d, t32_back,
-        )
+        # # ************************************************************
+        # self.next_section(
+        #     'show shape on simplified output map',
+        #     skip_animations=False,
+        # )
+        # # ************************************************************
+        # ac_all = VGroup(
+        #     aci_1,           aci_14, aci_15,
+        #     act_1, act_game, act_14, act_15
+        # )
+        # tensor_mobs = (
+        #     tin_raw, tin_norm, t32_offset, t32_prob, t32_merged_2d, t32_back,
+        # )
 
-        # show shapes for tensor mobs
-        ac_all.save_state()
-        self.play(ac_all.animate(
-            run_time=wt,
-        ).fade(0.8))
-        self.play(AnimationGroup(
-            *(ShowShape(mob, text_config=SMALL_SHAPE_TEXT_CONFIG)
-              for mob in tensor_mobs),
-            lag_ratio=0.5,
-            run_time=wt,
-        ))
-        self.wait()
+        # # show shapes for tensor mobs
+        # ac_all.save_state()
+        # self.play(ac_all.animate(
+        #     run_time=wt,
+        # ).fade(0.8))
+        # self.play(AnimationGroup(
+        #     *(ShowShape(mob, text_config=SMALL_SHAPE_TEXT_CONFIG)
+        #       for mob in tensor_mobs),
+        #     lag_ratio=0.5,
+        #     run_time=wt,
+        # ))
+        # self.wait()
 
-        # hide shapes
-        self.play(AnimationGroup(
-            *(HideShape(mob)
-              for mob in tensor_mobs),
-            lag_ratio=0.5,
-            run_time=wt,
-        ))
-        self.play(ac_all.animate(
-            run_time=wt,
-        ).restore())
+        # # hide shapes
+        # self.play(AnimationGroup(
+        #     *(HideShape(mob)
+        #       for mob in tensor_mobs),
+        #     lag_ratio=0.5,
+        #     run_time=wt,
+        # ))
+        # self.play(ac_all.animate(
+        #     run_time=wt,
+        # ).restore())
